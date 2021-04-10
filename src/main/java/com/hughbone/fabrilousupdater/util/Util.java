8ed6aead3bb5ca8cb3768b44288cd48cd72f3f88
HughBone <hughbone27@gmail.com>
2021-04-10 13:57:45 -0400

    jsonparser moved to util

--- src/main/java/com/hughbone/fabrilousupdater/util/Util.java
+++ src/main/java/com/hughbone/fabrilousupdater/util/Util.java
@@ -1,5 +1,8 @@
 package com.hughbone.fabrilousupdater.util;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mojang.bridge.game.GameVersion;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
@@ -8,10 +11,37 @@ import net.minecraft.MinecraftVersion;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Optional;
 
 public class Util {
+
+    private static String getJsonString(String sURL) {
+        try {
+            URL obj = new URL(sURL);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            return response.toString();
+        } catch (Exception e){
+            return null;
+        }
+    }
+    public static JsonArray getJsonArray(String sURL) {
+        String jsonStr = getJsonString(sURL);
+        return JsonParser.parseString(jsonStr).getAsJsonArray();
+    }
+    public static JsonObject getJsonObject(String sURL) {
+        String jsonStr = getJsonString(sURL);
+        return JsonParser.parseString(jsonStr).getAsJsonObject();
+    }
+
     public static String urlToString(String urlStr) throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
         URL url = new URL(urlStr);
@@ -52,5 +82,10 @@ public class Util {
         updateMinecraftVersion();
         return minecraftVersion;
     }
+
+
+
+
+
 
 }
