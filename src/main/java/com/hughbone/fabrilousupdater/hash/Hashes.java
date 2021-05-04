91b23d5205e98246582589fa35c9bf52006442dd
HughBone <hughbone27@gmail.com>
2021-05-04 13:08:48 -0400

    Get hash sh1 method

---
+++ src/main/java/com/hughbone/fabrilousupdater/hash/Hashes.java
@@ -0,0 +1,40 @@
+package com.hughbone.fabrilousupdater.hash;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Hashes {
+
+    public String getSH1(File file) {
+
+        // ex. --> String shString = getSHA1(new File("config/renammd.jar"));
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            FileInputStream fis = new FileInputStream(file);
+            byte[] dataBytes = new byte[1024];
+
+            int nread = 0;
+
+            while ((nread = fis.read(dataBytes)) != -1) {
+                md.update(dataBytes, 0, nread);
+            }
+
+            byte[] mdbytes = md.digest();
+
+            //convert the byte to hex format
+            StringBuffer sb = new StringBuffer("");
+            for (int i = 0; i < mdbytes.length; i++) {
+                sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16)
+                        .substring(1));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException | IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
