e57506e458984ef4f829735bf07a711a1f3826d2
HughBone <hughbone27@gmail.com>
2021-05-13 10:42:35 -0400

    removed modlist

--- src/main/java/com/hughbone/fabrilousupdater/FabrilousUpdater.java
+++ src/main/java/com/hughbone/fabrilousupdater/FabrilousUpdater.java
@@ -1,6 +1,5 @@
 package com.hughbone.fabrilousupdater;
 
-import com.hughbone.fabrilousupdater.command.ModListCommand;
 import com.hughbone.fabrilousupdater.command.ModUpdaterCommand;
 import net.fabricmc.api.ModInitializer;
 
@@ -9,8 +8,6 @@ public class FabrilousUpdater implements ModInitializer {
 
     @Override
     public void onInitialize() {
-
-        ModListCommand.register();
         ModUpdaterCommand.register();
     }
 }
